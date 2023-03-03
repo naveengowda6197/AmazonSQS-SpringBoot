1ccceea4eb1a6f68ddd6c30588de4b09f8979d5b
Naveena Krishna Gouda <55161387+naveengowda6197@users.noreply.github.com>
2023-03-03 20:59:41 +0530

    Update SurveyLaunch.java

--- AwsSQS/src/main/java/com/example/AwsSQS/Model/SurveyLaunch.java
+++ AwsSQS/src/main/java/com/example/AwsSQS/Model/SurveyLaunch.java
@@ -4,11 +4,7 @@ package com.example.AwsSQS.Model;
 import java.io.Serializable;
 
 public class SurveyLaunch implements Serializable {
-
-
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = -3874519901554516298L;
 
 	private String id;
@@ -42,26 +38,12 @@ public class SurveyLaunch implements Serializable {
 		this.name = name;
 	}
 
-	/**
-	 * @return the data
-	 */
 	public String getData() {
 		return data;
 	}
 
-	/**
-	 * @param data
-	 *            the data to set
-	 */
 	public void setData(String data) {
 		this.data = data;
 	}
-
-	/*
-	 * @Override public String toString() { return "ActiveMQDataForm [id=" + id +
-	 * ", name=" + name + ", data=" + data + "]"; }
-	 */
-	
-	
 
 }
